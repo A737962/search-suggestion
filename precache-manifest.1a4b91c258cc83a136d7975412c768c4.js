db4eab724196ea47ce216cb942aad55361b377f3
A737962 <aman.singh@atos.net>
2020-09-03 11:23:20 +0530

    Updates

---
+++ precache-manifest.1a4b91c258cc83a136d7975412c768c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9bd4309d14a5c1a339b2b723f692a8",
+    "url": "/search-suggestion/index.html"
+  },
+  {
+    "revision": "e42a8d3ba8873e445963",
+    "url": "/search-suggestion/static/css/main.1691dc48.chunk.css"
+  },
+  {
+    "revision": "4ca7c522b18e6a169088",
+    "url": "/search-suggestion/static/js/2.42cde3e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-suggestion/static/js/2.42cde3e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42a8d3ba8873e445963",
+    "url": "/search-suggestion/static/js/main.4e6d97ba.chunk.js"
+  },
+  {
+    "revision": "dd2f639e6517bede8312",
+    "url": "/search-suggestion/static/js/runtime-main.16bb2087.js"
+  }
+]);
